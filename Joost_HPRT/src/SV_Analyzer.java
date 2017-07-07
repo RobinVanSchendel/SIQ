5ae0b5e644e8e4969f0e40fb2a920d180da00acf
rvanschendel <R.van_Schendel@lumc.nl>
2017-07-07 10:47:18 +0200

    removed unused imports

--- Joost_HPRT/src/SV_Analyzer.java
+++ Joost_HPRT/src/SV_Analyzer.java
@@ -1,27 +1,7 @@
 import gui.GUI;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.NoSuchElementException;
-
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
-import org.biojava.bio.*;
-import org.biojava.bio.program.abi.*;
-import org.biojava.bio.seq.*;
-import org.biojava.bio.seq.impl.*;
-import org.biojava.bio.seq.io.*;
-import org.biojava.bio.symbol.*;
-import org.biojavax.bio.seq.RichSequence.IOTools;
-import org.jcvi.jillion.trace.chromat.Chromatogram;
-import org.jcvi.jillion.trace.chromat.ChromatogramFactory;
-
-import utils.CompareSequence;
-import utils.Utils;
 
 public class SV_Analyzer {
 
