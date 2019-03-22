d6cf42d846dd1502f41b8f968a5d7ca8bc57b9b9
rvanschendel <R.van_Schendel@lumc.nl>
2019-03-22 08:09:03 +0100

    file that executes blast and retrieves the result

---
+++ Joost_HPRT/src/utils/StreamGobbler.java
@@ -0,0 +1,45 @@
+package utils;
+
+import java.util.*;
+import java.io.*;
+class StreamGobbler extends Thread
+{
+    InputStream is;
+    String type;
+    HashMap<String, ArrayList<Blast>> blasts = new HashMap<String, ArrayList<Blast>>();
+    
+    StreamGobbler(InputStream is, String type)
+    {
+        this.is = is;
+        this.type = type;
+    }
+    
+    public void run()
+    {
+        try
+        {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line=null;
+            while ( (line = br.readLine()) != null){
+            	Blast b = Blast.parseBlast(line);
+            	if(blasts.containsKey(b.getId())) {
+            		blasts.get(b.getId()).add(b);
+            	}
+            	else {
+            		ArrayList<Blast> bl = new ArrayList<Blast>();
+            		bl.add(b);
+            		blasts.put(b.getId(), bl);
+            	}
+            	//System.out.println(line);
+                //System.out.println(type + ">" + line);
+            }
+            } catch (IOException ioe)
+              {
+                ioe.printStackTrace();  
+              }
+    }
+    public HashMap<String, ArrayList<Blast>> getBlastResult(){
+    	return blasts;
+    }
+}
