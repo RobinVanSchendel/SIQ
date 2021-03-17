c1c096ce8c56e1a7bfa457524a89a431d023e589
rvanschendel <R.van_Schendel@lumc.nl>
2021-03-17 22:16:45 +0100

    version bump

--- Joost_HPRT/src/main/SV_Analyzer.java
+++ Joost_HPRT/src/main/SV_Analyzer.java
@@ -6,7 +6,7 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class SV_Analyzer {
-	public final static String VERSION = "4.3";
+	public final static String VERSION = "4.6";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
