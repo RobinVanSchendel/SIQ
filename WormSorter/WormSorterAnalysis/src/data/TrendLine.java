af903da47e837d291b49cac3909b074a9b48b077
rvanschendel <R.van_Schendel@lumc.nl>
2021-09-15 09:53:54 +0200

    removed WormSorter files

--- WormSorter/WormSorterAnalysis/src/data/TrendLine.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public interface TrendLine {
-    public void setValues(double[] y, double[] x); // y ~ f(x)
-    public double predict(double x); // get a predicted y for a given x
-	public void setDeviation(double d);
-}
