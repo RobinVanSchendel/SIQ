afbecfca17204d71892a73b68cdc5ff41d77b8f6
rvanschendel <R.van_Schendel@lumc.nl>
2022-09-30 07:40:11 +0200

    exit with -1 in case of error

--- SIQ/src/utils/MyError.java
+++ SIQ/src/utils/MyError.java
@@ -4,7 +4,7 @@ public class MyError {
 	public static void err(String msg, boolean exit) {
 		System.err.println(msg);
 		if(exit) {
-			System.exit(0);
+			System.exit(-1);
 		}
 	}
 	public static void err(String msg) {
